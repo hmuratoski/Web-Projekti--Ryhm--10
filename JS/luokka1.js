f474a862ea7902f88759bee4a4748fe1d51b3ed1
kerlinurk <nurk.kerli@gmail.com>
2022-04-28 15:24:13 +0300

    kunhan kokeillu omille sivuille juttuja

---
+++ JS/luokka1.js
@@ -0,0 +1,13 @@
+
+//KOKEILU
+$( document ).ready(function() {
+    $("#o1").click(function (){
+    $("Helsinki").show();
+    $("Espoo").hide();
+    });
+    
+    $("#o2").click(function (){
+      $("#a2").show();
+      $("#a1").hide();
+    });
+  });
