f0483831bb8397cfffeada2cfb5c1b8b2010b99e
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-05-04 20:07:22 +0300

    Kirjoitusvirheitä korjattu;

--- JS/luokka1.js
+++ JS/luokka1.js
@@ -78,12 +78,61 @@ function clearStatusClass(element){
     element.classList.remove("correct")
     element.classList.remove("wrong")
 }
-const questions =  [
+let questions = [
     {
-        question: "Mikä on Suomen pääkaupunki?" ,
-        answers: [
-            { text: "Helsinki", correct: true }
-            {text: "Kuopio", correct: false }
-        ]
-    }
-]
+    numb: 1,
+    question: "Mikä oli Suomen alkuperäinen pääkaupunki?",
+    answer: "Turku",
+    options: [
+      "Turku",
+      "Helsinki",
+      "Tampere",
+      "Jyväskylä"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Kunka monta kansallispuistoja on Suomessa?",
+    answer: "41",
+    options: [
+      "12",
+      "41",
+      "100",
+      "33"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Mikä on Suomen syvin järvi?",
+    answer: "Päijanne",
+    options: [
+      "Päijanne",
+      "Saimaa",
+      "Haukivesi",
+      "Inarijärvi "
+    ]
+  },
+    {
+    numb: 4,
+    question: "Mikä on Suomen pohjoisin kylä?",
+    answer: "Nuorgam",
+    options: [
+      "Inari",
+      "Sodankylä",
+      "Angeli",
+      "Nuorgam"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Mikä on väkimäärältään pienin kaupunki?",
+    answer: "Kuopio",
+    options: [
+      "Mantsälä",
+      "Kitee",
+      "Kuopio",
+      "Lahti"
+    ]
+  },
+  
+];
