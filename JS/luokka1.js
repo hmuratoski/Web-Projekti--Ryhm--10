35b764e1755e9e3bbb83b48110b0879b5f7817ca
hmuratoski <98337481+hmuratoski@users.noreply.github.com>
2022-05-04 20:18:23 +0300

    kokeilu;

--- JS/luokka1.js
+++ JS/luokka1.js
@@ -79,15 +79,37 @@ function clearStatusClass(element){
     element.classList.remove("wrong")
 }
 
-  const questions =  [
+const questions = [
     {
-        question: " What is 2 + 2??  " ,
-        question: "Mikä on Suomen pääkaupunki?" ,
-        answers: [
-            { text: " 4 ", correct: true } ,
-            {text: " 22 ", correct: false }
-            { text: "Helsinki", correct: true }
-            {text: "Kuopio", correct: false }
-        ]
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
     }
-]
+  ]
