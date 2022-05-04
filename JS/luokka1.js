28a17b6adb2d5516393be7a861db6de6aa86d856
kerlinurk <nurk.kerli@gmail.com>
2022-05-04 14:19:08 +0300

    oma sivu

---
+++ JS/luokka1.js
@@ -0,0 +1,20 @@
+const startButton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById
+("question-container")
+
+startButton.addEventListener("click", startGame)
+
+function startGame() {
+console.log("started")
+startButton.classList.add("hide")
+quedstionContainerElement.classList.remove("hide")
+}
+
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
